a43fa9d53f825535bdd37428702ddd4f1440af96
Pankaj <pankaj.54666@gmail.com>
2020-03-26 20:27:07 +0530

    new view + HTML added

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views import View
+from blog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.post_list, name='post_list'),
 ]
